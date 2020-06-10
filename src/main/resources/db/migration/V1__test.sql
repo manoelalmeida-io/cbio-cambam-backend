bd5459892f45f279e4339efbb8280e91708934e1
Manoel Almeida <manoel.almeida@bandtec.com.br>
2020-06-10 10:33:03 -0300

    Setup repository

---
+++ src/main/resources/db/migration/V1__test.sql
@@ -0,0 +1,6 @@
+/**
+ * Author:  manoel
+ * Created: 9 de jun de 2020
+ */
+
+create table test;
---
+++ src/test/java/com/cbio/cambam/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbio.cambam.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
